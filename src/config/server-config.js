b97122b94349db7b1242b02f3dcddf3263574e4d
Jash <jashhinger62@gamil.com>
2023-06-02 12:14:26 +0530

    Added encryption in signup

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-module.exports={
-    PORT :process.env.PORT
+module.exports = {
+  PORT: process.env.PORT,
+  SALT_ROUNDS: process.env.SALT_ROUNDS,
 }
